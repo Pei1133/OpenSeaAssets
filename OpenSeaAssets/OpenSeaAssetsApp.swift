b71d181357c042ea325db1c27521d38ce40db588
Pei Huang <peihuang@nine-yi.com>
2022-02-23 16:36:55 +0800

    Initial Commit

---
+++ OpenSeaAssets/OpenSeaAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSeaAssetsApp.swift
+//  OpenSeaAssets
+//
+//  Created by PeiChun on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenSeaAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
