f5ecd2144d77a23e5d9a798eafbd79750cf74f46
Pei Huang <peihuang@nine-yi.com>
2022-02-23 20:16:55 +0800

    [feat] add assetDetailView

---
+++ OpenSeaAssets/AssetDetailView.swift
@@ -0,0 +1,40 @@
+//
+//  AssetDetailView.swift
+//  OpenSeaAssets
+//
+//  Created by PeiChun on 2022/2/23.
+//
+
+import Foundation
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct AssetDetailView: View {
+    var asset: Asset
+    init(asset: Asset) {
+        self.asset = asset
+    }
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView {
+                    VStack {
+                        WebImage(url: (asset.image_url ?? URL(string: "")))
+                            .resizable()
+                            .scaledToFit()
+                        Text(asset.name)
+                        Text(asset.description ?? "")
+                    }
+                }
+                if let url = asset.permalink {
+                    Link(destination: url, label: {
+                        Text("permalink")
+                    })
+                }
+            }
+            .navigationTitle(asset.collection?.name ?? "")
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
+    }
+}
