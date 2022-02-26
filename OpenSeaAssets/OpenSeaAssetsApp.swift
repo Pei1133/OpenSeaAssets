0e726343f4c71ff2f93d38180a802b83b8a12d14
Pei Huang <peihuang@nine-yi.com>
2022-02-26 17:40:38 +0800

    [chore] rename AssetView

--- OpenSeaAssets/OpenSeaAssetsApp.swift
+++ OpenSeaAssets/OpenSeaAssetsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct OpenSeaAssetsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AssetView()
         }
     }
 }
--- OpenSeaAssets/View/AssetView.swift
+++ OpenSeaAssets/View/AssetView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
-struct ContentView: View {
+struct AssetView: View {
     @ObservedObject var model = OpenSeaModel()
     private var columnGrid: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
     
@@ -49,6 +49,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AssetView()
     }
 }
